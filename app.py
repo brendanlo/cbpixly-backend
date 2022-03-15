ad8fbc4cf96e809f7fcf9bfc151611ff0818a6a3
prokid20000 <95936374+prokid20000@users.noreply.github.com>
2022-03-15 18:26:54 -0400

    working on backend model and app

--- app.py
+++ app.py
@@ -1,4 +1,30 @@
 from flask import Flask
+import os
+
+from models import db, connect_db
+
 
 app = Flask(__name__)
+app.config["SECRET_KEY"]= os.environ["SECRET_KEY"]
 
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///pixly"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+connect_db(app)
+db.create_all()
+
+
+
+
+@app.get("/photos")
+def get_all_photos():
+    """Get all photos"""
+
+    return jsonify([photos])
+
+
+@app.post("/photos")
+def create_photo():
+    """Add new photo"""
+
+    return 
