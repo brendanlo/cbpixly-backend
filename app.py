f94595d3d20c52a9f3108dd3b3e140034e7669d6
brendan <lobrendan@gmail.com>
2022-03-17 09:50:23 -0700

    added aws uploading, need to work on return values

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask_cors import CORS
 import os
 import boto3
 from models import db, connect_db, Photo
+from werkzeug.utils import secure_filename
+from botocore.exceptions import ClientError
+
+
 
 
 app = Flask(__name__)
@@ -12,8 +16,13 @@ app.config["SECRET_KEY"]= os.environ["SECRET_KEY"]
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///pixly"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+
 BUCKET_NAME=os.environ["BUCKET_NAME"]
-s3 = boto3.client('s3')
+s3 = boto3.client(
+    's3', 
+    aws_access_key_id=os.environ['aws_access_key_id'],
+    aws_secret_access_key=os.environ['aws_secret_access_key']
+    )
 
 connect_db(app)
 db.create_all()
@@ -38,22 +47,20 @@ def create_photo():
         :return: True if file was uploaded, else False
     """
 
-  
-    # file = request.files['file']
-    # file_name = secure_filename(file.filename)
-    # if object_name is None:
-    #     object_name = os.path.basename(file_name)
+    new_file = request.files['file']
+    file_name = secure_filename(new_file.filename)
+    object_name = os.path.basename(file_name)
     # #TODO add metadata here
 
-    # s3_client = boto3.client('s3')
-    # try:
-    #     response = s3_client.upload_file(file_name, BUCKET_NAME, object_name)
-    # except ClientError as e:
-    #     logging.error(e)
-    #     return False
-    breakpoint()
+    # breakpoint()
+    try:
+        response = s3.upload_fileobj(new_file, BUCKET_NAME, object_name)
+        # find the AWS URL > DB
+        breakpoint()
+    except ClientError as e:
+        logging.error(e)
+        return False
     # #TODO add file location to DB
-    # print("requet.files['file'] is!!!!!: ", request.files['file'])
-    print("request is: ", request)
+    print("request.files['file'] is: ", request.files['file'])
     
     return "Hello"
